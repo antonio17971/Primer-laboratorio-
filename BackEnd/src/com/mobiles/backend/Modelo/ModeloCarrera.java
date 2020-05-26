b9e92fb5254e078c9e5f2f70a413f85eee19144d
antonio17971 <39813645+antonio17971@users.noreply.github.com>
2020-05-25 18:18:35 -0600

    carpeta

--- BackEnd/src/com/mobiles/backend/Modelo/ModeloCarrera.java
+++ BackEnd/src/com/mobiles/backend/Modelo/ModeloCarrera.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.moviles.backend.Modelo;
+package com.mobiles.backend.Modelo;
 
 import com.mobiles.backend.AccesoADatos.GlobalException;
 import com.mobiles.backend.AccesoADatos.NoDataException;
